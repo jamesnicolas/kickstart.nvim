f64653d4a9341cff738762cf6adf82402bf08308
James Nicolas <jamesnicolas@me.com>
2024-01-18 13:31:51 -0500

    okay fix it for real

--- init.lua
+++ init.lua
@@ -606,7 +606,7 @@ cmp.setup {
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete {},
-    -- ['<CR>'] = cmp.mapping.confirm { select = false },
+    ['<CR>'] = cmp.mapping.confirm { select = false },
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
